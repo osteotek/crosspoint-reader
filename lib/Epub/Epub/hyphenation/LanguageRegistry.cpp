999587d5c4347fd716758f4b0db600a5b14450dd
Arthur Tazhitdinov <lisnake@gmail.com>
2026-01-26 21:04:42 +0500

    feat: Add support for German hyphenation based on configuration

--- lib/Epub/Epub/hyphenation/LanguageRegistry.cpp
+++ lib/Epub/Epub/hyphenation/LanguageRegistry.cpp
@@ -4,7 +4,9 @@
 #include <array>
 
 #include "HyphenationCommon.h"
+#ifndef DISABLE_GERMAN_HYPHENATION
 #include "generated/hyph-de.trie.h"
+#endif
 #include "generated/hyph-en.trie.h"
 #include "generated/hyph-fr.trie.h"
 #include "generated/hyph-hu.trie.h"
@@ -15,18 +17,31 @@ namespace {
 // English hyphenation patterns (3/3 minimum prefix/suffix length)
 LanguageHyphenator englishHyphenator(en_us_patterns, isLatinLetter, toLowerLatin, 3, 3);
 LanguageHyphenator frenchHyphenator(fr_patterns, isLatinLetter, toLowerLatin);
+#ifndef DISABLE_GERMAN_HYPHENATION
 LanguageHyphenator germanHyphenator(de_patterns, isLatinLetter, toLowerLatin);
+#endif
 LanguageHyphenator hungarianHyphenator(hu_patterns, isLatinLetter, toLowerLatin);
 LanguageHyphenator russianHyphenator(ru_ru_patterns, isCyrillicLetter, toLowerCyrillic);
 
+#ifdef DISABLE_GERMAN_HYPHENATION
+using EntryArray = std::array<LanguageEntry, 4>;
+#else
 using EntryArray = std::array<LanguageEntry, 5>;
+#endif
 
 const EntryArray& entries() {
+#ifdef DISABLE_GERMAN_HYPHENATION
+  static const EntryArray kEntries = {{{"english", "en", &englishHyphenator},
+                                       {"french", "fr", &frenchHyphenator},
+                                       {"hungarian", "hu", &hungarianHyphenator},
+                                       {"russian", "ru", &russianHyphenator}}};
+#else
   static const EntryArray kEntries = {{{"english", "en", &englishHyphenator},
                                        {"french", "fr", &frenchHyphenator},
                                        {"german", "de", &germanHyphenator},
                                        {"hungarian", "hu", &hungarianHyphenator},
                                        {"russian", "ru", &russianHyphenator}}};
+#endif
   return kEntries;
 }
 
